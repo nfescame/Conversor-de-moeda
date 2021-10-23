91d751f81cacd2f0117ba07f38977d516b09a427
nilton <nfescame@gmail.comq:qclear>
2021-10-23 03:06:05 -0300

    :package:fist commit conversor funcionando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Conversor from "./components/Conversor.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='App'>
+      <h1>Conversor de moeda</h1>
+      <div className='linha'>
+        <Conversor moedaA='USD' moedaB='BRL' />
+        <Conversor moedaA='BRL' moedaB='USD' />
+      </div>
+      <div className='linha'>
+        <Conversor moedaA='CAD' moedaB='BRL' />
+        <Conversor moedaA='BRL' moedaB='CAD' />
+      </div>
+      <div className='linha'>
+        <Conversor moedaA='EUR' moedaB='BRL' />
+        <Conversor moedaA='BRL' moedaB='EUR' />
+      </div>
+      <a>
+        Photo by
+        <a href='https://unsplash.com/@jjying?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>
+          JJ Ying
         </a>
-      </header>
+        on
+        <a href='https://unsplash.com/s/photos/abstract?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>
+          Unsplash
+        </a>
+      </a>
     </div>
   );
 }
